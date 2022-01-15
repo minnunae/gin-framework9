1b2f8a6aafc42243b65fd9b91327f4fe45c64180
minnunae <69183312+minnunae@users.noreply.github.com>
2022-01-15 10:15:40 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/patipan-patisampita/gin-framework9/configs"
+	"github.com/patipan-patisampita/gin-framework9/routes"
+)
+
+func main() {
+	router := SetupRouter()
+	router.Run(":" + os.Getenv("GO_PORT")) //listen and server on 0.0.0.0:8080
+}
+
+func SetupRouter() *gin.Engine{
+	//Connection db
+	configs.Connection()
+	router := gin.Default()
+
+	apiV1 := router.Group("/api/v1")//127.0.0.1:3000/api/v1
+
+	routes.InitHomeRoutes(apiV1)
+	routes.InitUserRoutes(apiV1)//127.0.0.1:3000/api/v1/users/register
+	return router
+}
---
+++ user.go
@@ -0,0 +1,59 @@
+package usercontroller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/patipan-patisampita/gin-framework9/configs"
+	"github.com/patipan-patisampita/gin-framework9/models"
+)
+
+func GetAll(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data new": "users",
+	})
+}
+
+func Register(c *gin.Context) {
+	var input InputRegister
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.User{
+		Fullname: input.Fullname,
+		Email:    input.Email,
+		Password: input.Password,
+	}
+	result := configs.DB.Debug().Create(&user)
+
+	//db error
+	if result.Error !=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":result.Error})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "สมัครสมาชิกสำเร็จแล้ว",
+	})
+}
+
+func Login(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "login",
+	})
+}
+
+func GetById(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "id",
+	})
+}
+
+func SearchByFullName(c *gin.Context) {
+	fullname := c.Query("fullname")
+	c.JSON(200, gin.H{
+		"data": fullname,
+	})
+}
